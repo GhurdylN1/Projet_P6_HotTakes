9182cfb03fc530bb2712c98daa1e8fefb45b0e21
Ghurdyl <ghurdyl@gmail.com>
2022-09-15 12:36:18 +0200

    ajout du module dotenv pour connexion MongoDB

--- backend/app.js
+++ backend/app.js
@@ -1,23 +1,33 @@
+const dotenv = require('dotenv').config('../.env');
+
 const express = require('express');
+const mongoose = require('mongoose');
+
+const sauceRoutes = require('./routes/sauce');
+const userRoutes = require('./routes/user');
+
+//&w=majority` retiré de l'addresse car ça fasait bug le signin, l'user etait bien ajouté à la DB mais affichage erreur 400. 
+mongoose.connect(`mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_CLUSTER_NAME}.mongodb.net/${process.env.MONGODB_DATABASE_NAME}?retryWrites=true`, 
+{useNewUrlParser: true,
+  useUnifiedTopology: true })
+.then(() => console.log('Connexion à MongoDB réussie !'))
+.catch((error) => console.log(error));
+//.catch(() => console.log('Connexion à MongoDB échouée !'));
 
 const app = express();
+const path = require('path');
 
 app.use((req, res, next) => {
-    console.log('Requête reçue !');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
-});
+  });
 
-app.use((req, res, next) => {
-    res.status(201);
-    next();
-});
+app.use(express.json());
 
-app.use((req, res, next) => {
-    res.json({ message: 'Votre requête a bien été reçue !'});
-});
-
-app.use((req, res) => {
-    console.log('Réponse envoyée avec succès !');
-});
+//app.use('/api/sauces', sauceRoutes);
+app.use('/api/auth', userRoutes);
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 module.exports = app;
